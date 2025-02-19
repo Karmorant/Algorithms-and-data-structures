c1dd34177e6293ec57586c7341b79cca04a0db3d
Vladislav <diz28600@gmail.com>
2025-02-19 16:48:14 +0300

    Input from stdin

--- Task_1/test_canonicalize_path.c
+++ Task_1/test_canonicalize_path.c
@@ -1,30 +1,22 @@
+#include "canonicalize_path.h"
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 
 extern char* canonicalize_path(const char *path);
 
-void test_canonicalize_path() {
-    // Тест 1
-    assert(strcmp(canonicalize_path("/"), "/") == 0);
-
-    // Тест 2
-    assert(strcmp(canonicalize_path("/home/"), "/home") == 0);
-
-    // Тест 3
-    assert(strcmp(canonicalize_path("/home////////////////////Downloads/////"), "/home/Downloads") == 0);
-
-    // Тест 4
-    assert(strcmp(canonicalize_path("/home/./Downloads/Films/../Stories/"), "/home/Downloads/Stories") == 0);
-
-    // Тест 5 (попытка выйти выше корня)
-    printf("Expecting an error for the following test:\n");
-    canonicalize_path("/../");
-
-    printf("All tests passed!\n");
-}
-
 int main() {
-    test_canonicalize_path();
-    return 0;
+    char input[PATH_MAX];
+
+    if (fgets(input, PATH_MAX, stdin)) {
+        input[strcspn(input, "\n")] = '\0';
+        char *canonical_path = canonicalize_path(input);
+        printf("%s\n", canonical_path);
+        free(canonical_path);
+    } else {
+        fprintf(stderr, "Error reading input from stdin.\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
